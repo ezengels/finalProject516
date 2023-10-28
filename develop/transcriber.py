5f055ce3abb77531fc96f106da2bcc81d0432143
Ezra Engels <ezengels@iu.edu>
2023-10-27 23:15:57 -0400

    retrieving image file paths

--- develop/transcriber.py
+++ develop/transcriber.py
@@ -1,9 +1,20 @@
 import os
 
+images = []
+
 path = os.getcwd()
 dir_list = os.listdir(path)
 paths = [path + '/' + sub + '/' for sub in dir_list]
 datafolder = paths[1]
 datafolders = os.listdir(datafolder)
+for folder in datafolders:
+    datafiles = []
+    datafiles.append(datafolder + folder + '/')
+    for subfolder in datafiles:
+        pathslist = []
+        files = os.listdir(subfolder)
+        for file in files:
+            filepath = subfolder + '/' + file
+            images.append(filepath)
 
-print(datafolders)
+print(images)
