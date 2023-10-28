f0b7b770904449d1e7cc9fc0312a3bd2a72f0587
Ezra Engels <ezengels@iu.edu>
2023-10-28 00:42:31 -0400

    started image processing, imports not working

--- develop/transcriber.py
+++ develop/transcriber.py
@@ -1,4 +1,6 @@
 import os
+import cv2
+import pytesseract
 
 images = []
 
@@ -17,4 +19,11 @@ for folder in datafolders:
             filepath = subfolder + '/' + file
             images.append(filepath)
 
-print(images)
+for image in images:
+	image = cv2.imread(image)
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	image = cv2.bilateralFilter(image, 3, 10, 10)
+	def threshold(image):
+		img = cv2.threshold(img, 0, 230, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        return image
+	print(pytesseract.image_to_string(Image.open(image)))
