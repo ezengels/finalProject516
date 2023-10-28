eafce8de283c0b678a1800a7e1011b87e93315ea
Ezra Engels <ezengels@iu.edu>
2023-10-28 12:35:17 -0400

    print removed

--- develop/transcriber.py
+++ develop/transcriber.py
@@ -11,8 +11,7 @@ datafolder = paths[1]
 for root, dirs, files in os.walk(datafolder, topdown = True, onerror = None):
 	for name in files:
 		images.append(os.path.join(root, name))
-
-print(images)
+		
 for image in images:
 	image = cv2.imread(image)
 	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
