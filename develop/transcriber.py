717d01e9bcfa752795bf0970f39cccc909ee6fd0
Ezra Engels <ezengels@iu.edu>
2023-10-28 12:28:30 -0400

    walk method added instead of 3 listdir for loops

--- develop/transcriber.py
+++ develop/transcriber.py
@@ -8,17 +8,11 @@ path = os.getcwd()
 dir_list = os.listdir(path)
 paths = [path + '/' + sub + '/' for sub in dir_list]
 datafolder = paths[1]
-datafolders = os.listdir(datafolder)
-for folder in datafolders:
-    datafiles = []
-    datafiles.append(datafolder + folder + '/')
-    for subfolder in datafiles:
-        pathslist = []
-        files = os.listdir(subfolder)
-        for file in files:
-            filepath = subfolder + '/' + file
-            images.append(filepath)
+for root, dirs, files in os.walk(datafolder, topdown = True, onerror = None):
+	for name in files:
+		images.append(os.path.join(root, name))
 
+print(images)
 for image in images:
 	image = cv2.imread(image)
 	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
