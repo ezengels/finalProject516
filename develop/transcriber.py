76851178123ec4e7dbd55dd7422d3f262f9d865d
Ezra Engels <ezengels@iu.edu>
2023-10-29 21:46:38 -0400

    Successfully returning readable pdfs with unique identifiers

--- develop/transcriber.py
+++ develop/transcriber.py
@@ -1,6 +1,6 @@
 import os
-#import cv2
-#import pytesseract
+import cv2
+import pytesseract
 
 imagepaths = []
 pdfnames = []
@@ -18,13 +18,12 @@ for i in imagepaths:
 	i = i.replace(".jpg", "")
 	pdfnames.append(i)
 		
-"""for image in imagepaths:
+for image in imagepaths:
+	l = imagepaths.index(image)
 	image = cv2.imread(image)
 	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	image = cv2.bilateralFilter(image, 3, 10, 10)
 	# image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)[1]
-    
 	pdf = pytesseract.image_to_pdf_or_hocr(image, extension='pdf')
-	with open('test.pdf', 'w+b') as f:
-		f.write(pdf)"""
-
+	with open(pdfnames[l] + '.pdf', 'w+b') as f:
+		f.write(pdf)
