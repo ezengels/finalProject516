585555cb1dd1eaf666fc5ad2efd604ab9f83596b
Ezra Engels <ezengels@iu.edu>
2023-11-04 15:38:23 -0400

    broken pdf output

--- develop/transcriber.py
+++ develop/transcriber.py
@@ -8,11 +8,17 @@ imagepaths = []
 pathsshort = []
 pdfnames = []
 # Map1 Script
+inputfolder = ''
+
 path = os.getcwd()
 dir_list = os.listdir(path)
-paths = [path + '/' + sub + '/' for sub in dir_list]
-datafolder = paths[1]
-for root, dirs, files in os.walk(datafolder, topdown = True, onerror = None):
+paths = [sub + '/' for sub in dir_list]
+
+for path in paths:
+	if re.search("input", path):
+		inputfolder = inputfolder + path
+
+for root, dirs, files in os.walk(inputfolder, topdown = True, onerror = None):
 	for name in files:
 		imagepaths.append(os.path.join(root, name))
 
@@ -25,7 +31,10 @@ for i in imagepaths:
 			g = g.replace("/","")
 			g = g.replace(".jpg", "")
 			pdfnames.append(g)
+
 # Reduce1 Script
+transcribed = ''
+
 for image in imagepaths:
 	l = imagepaths.index(image)
 	image = cv2.imread(image)
@@ -33,5 +42,8 @@ for image in imagepaths:
 	image = cv2.bilateralFilter(image, 3, 10, 10)
 	# image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)[1]
 	pdf = pytesseract.image_to_pdf_or_hocr(image, extension='pdf')
-	with open(pdfnames[l] + '.pdf', 'w+b') as f:
+	for path in paths:
+		if re.search("transcribed", path):
+			transcribed = transcribed + path
+	with open('./' + pdfnames[l] + '.pdf', 'w+b') as f:
 		f.write(pdf)
